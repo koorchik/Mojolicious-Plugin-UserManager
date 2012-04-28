b06539ea9baea321728ee887d11020509f1f12f5
Viktor Turskyi <viktor@webbylab.com>
2012-04-29 00:52:10 +0300

    better duplication field

--- lib/Mojolicious/Plugin/UserManager.pm
+++ lib/Mojolicious/Plugin/UserManager.pm
@@ -287,7 +287,7 @@ sub _apply_conf_defaults {
 
     my %fields;
     foreach my $f ( @{ $conf->{fields} } ) {
-        croak "Field [$_->{name}] duplication\n" if $fields{ $f->{name} };
+        croak "UM field [$f->{name}] exists twice\n" if $fields{ $f->{name} };
         $fields{ $f->{name} }++;
     }
 
